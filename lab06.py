d66c3592847552ab9e48c358ee5485ae78ef23b2
Marie-Ange Chhuon <marieangechhuon@gmail.com>
2025-10-24 09:45:29 -0400

    Answer questions 2,3
    We answered questions 2 and 3 of the lab 6 (plotted graphs).

--- lab06.py
+++ lab06.py
@@ -16,7 +16,7 @@ df = pd.read_csv("wdi_wide.csv")
 print(df)
 print(df.info()) #10 empty values for the column "Physicians" and no empty value for the column "Population"
 print(df.nunique())
-print(df.describe())
+print(df.describe()) #This function provides a statistical analysis of the dataset (mean, std, min, max and confidence intervals) 
 
 #6
 GNI_per_capita = df["GNI per capita"] = (df["GNI"] / df["Population"]).round(2)
@@ -39,7 +39,30 @@ for i in filtered_women_expectancy["Country Name"]:
     print(i)
     
 #Part 4
+#1
 sns.relplot(data=df,
             x="GNI per capita",
             y="Life expectancy, female")
-    
+#2
+sns.relplot(data=df,
+            x="Region",
+            y="Life expectancy, female",
+            hue="GNI per capita",)  
+sns.relplot(data=df,
+            x="Life expectancy, male" ,
+            y="Region",
+            hue="GNI per capita",)  
+#3
+sns.relplot(data=df,
+            kind="line",
+            x="Region",
+            y="Life expectancy, female",
+            errorbar="sd",)     
+sns.relplot(data=df,
+            kind="line",
+            x="Region",
+            y="Life expectancy, male",
+            errorbar="sd",)   
+
+            x="Region",
+            y="Life expectancy, male",)   
